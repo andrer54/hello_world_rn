230f26f83eb10d1ae0c6916cdfac13e8ea969d40
andrer54 <andrer54@gmail.com>
2020-12-15 15:31:01 -0200

    ajustes2

--- App.js
+++ App.js
@@ -38,16 +38,8 @@ const App: () => React$Node = () => {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
+
+
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
@@ -57,23 +49,9 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     color: Colors.black,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+
+
+
 });
 
 export default App;
